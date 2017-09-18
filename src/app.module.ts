246acd8fbca6a3ee9ccb5f8c381930999be06b3a
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-09-18 11:35:44 +0200

    premier component

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        AppComponent
+    ],
     imports: [
         BrowserModule
     ],
